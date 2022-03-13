0b56c2f50a784a089066e5c12285075186203c31
Carlos Garcia <C@CA-GA.local>
2022-03-13 22:23:16 +0800

    Added misc

--- src/Routes/pages/main.js
+++ src/Routes/pages/main.js
@@ -5,7 +5,7 @@ export default class Main extends Component {
   render() {
     return (
     <StyledMain>
-        <div>MAIN PAGE HERE</div> 
+        <div>MAIN PAGE HERE - FUCK U</div> 
     </StyledMain>
     )
   }
